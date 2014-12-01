381a0772d22cedd5482f8ecb83ca779342ad5b68
FelixLC <felix.lechevallier@gmail.com>
2014-12-01 17:41:53 +0100

    test=true

--- build/src/common/clientfactory/settings.js
+++ build/src/common/clientfactory/settings.js
@@ -13,7 +13,7 @@
   function settings () {
     var service = {
       production: true,
-      test: false,
+      test: true,
       apiEndPoint: 'http://127.0.0.1:8000/api'
       // apiEndPoint: 'https://api.vinify.co/api'
       // apiEndPoint: 'https://backoffice.vinify.co/api'
